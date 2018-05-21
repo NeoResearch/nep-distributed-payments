3db7ba9fa336bbec942c9b0d9cd49b397d4b45a8
Vitor <vncoelho@gmail.com>
2018-05-21 12:53:06 -0300

    Descriptions for our workingPrototype

--- workingPrototype.cs
+++ workingPrototype.cs
@@ -1,7 +1,6 @@
 Congratulations to all of us! 
 Good compreenshion.
 
-
 ------------------------------------------------------------------------------------------------
 Step 1 - Deploy the contract, we need storage
 
@@ -9,7 +8,7 @@ Step 2 - Make a transfer to the address of the deployed contract
 
 Step 3 - Call "method" ["address","values(not used yet)+00000000",b"utxo" (reverted)] 
 EXAMPLES IN PYTHON:
-"5265636f72645472616e73666572416e645554584f" ["AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y","1500000000",b"45a6d2fa9d171ad1c9b9f71d15499231dc4a8d54be3ce42f17ed5bc4a7e85e26"]
+"5265636f72645472616e73666572416e645554584f" ["AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y","1500000000",b"211eb9cb250574f3e061e8506ebbbee91b3aebbc0a1f8018e7ebaecf907e4070"]
 
 This step register that AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y will receive 1 NEO OR GAS from UTXO 0056facaee834ad514b60d06c1dd43f8741dd9c4c245b22b8a1d75a11c7228df
 TODO: Limit the Neo or Gas by asset id registered.
@@ -31,7 +30,6 @@ Amount of change to the contract
 
 ------------------------------------------------------------------------------------------------ 
 
-
 using Neo.SmartContract.Framework;
 using Neo.SmartContract.Framework.Services.Neo;
 using Neo.SmartContract.Framework.Services.System;
@@ -47,33 +45,34 @@ namespace NeoContract1
     {
 
         private static readonly byte[] neo_asset_id = { 155, 124, 255, 218, 166, 116, 190, 174, 15, 147, 14, 190, 96, 133, 175, 144, 147, 229, 254, 86, 179, 74, 92, 34, 12, 205, 207, 110, 252, 51, 111, 197 };
+	private static readonly byte[] gas_asset_id = { 231, 45, 40, 105, 121, 238, 108, 177, 183, 230, 93, 253, 223, 178, 227, 132, 16, 11, 141, 20, 142, 119, 88, 222, 66, 228, 22, 139, 113, 121, 44, 96 };
+
         // TODO - Include GAS IS for limiting the withdraw of neo or gas
       
         public static object Main(string operation, params object[] args)
         {
             if (Runtime.Trigger == TriggerType.Verification)
             {
-               byte[] utxoKey = new byte[] { 3 }; // first status -> Waiting editor acceptance
-               byte[] valueKey = new byte[] { 2 }; // first status -> Waiting editor acceptance
-               byte[] addresKey = new byte[] { 1 }; // first status -> Waiting editor acceptance
+               //TODO Include key for asset ID (neo or gas)
+               byte[] utxoKey = new byte[] { 3 }; 
+               byte[] valueKey = new byte[] { 2 };
+               byte[] addresKey = new byte[] { 1 };
                byte[] addressToReward = Storage.Get( Storage.CurrentContext, addresKey );
                byte[] valueToReward = Storage.Get( Storage.CurrentContext, valueKey );
                byte[] utxoWasRegistered = Storage.Get( Storage.CurrentContext, utxoKey );
                //Runtime.Notify("addresKey is:  " + addresKey);
                //Runtime.Notify("addressToReward is:  " + addressToReward);
-               //Runtime.Notify("sender is:  " + GetSender());
                
                Transaction tx = (Transaction)ExecutionEngine.ScriptContainer;
                TransactionOutput[] outputs = tx.GetOutputs();
                TransactionInput[] inputs = tx.GetInputs();
                
+	       //In this first prototype, the output 0 should be the one that was registered and will withdraw
                bool outputAddressWasRegistered = (outputs[0].ScriptHash.AsBigInteger() == addressToReward.AsBigInteger());
                bool theUTXOWasRegistered = (inputs[0].PrevHash == utxoWasRegistered);
-
                ulong valueThatIsBeingAskedToBeWithdrew = GetContributeValue(tx, addressToReward);
                bool theRequestValueWasRegistered = (valueThatIsBeingAskedToBeWithdrew == valueToReward.AsBigInteger());
        
-               //TODO - An additional check is still required
                // If outputs > 0, means that more people will receive from this withdraw. In this first version only the contract can be the second
                // Letś say that the second should always be the change, if more than 1 outcome is used
                int i =0;
@@ -81,12 +80,12 @@ namespace NeoContract1
                {
                    if(i==1)
                    {
-                       if(outputs[1].ScriptHash.AsBigInteger() != GetReceiver().AsBigInteger()  )
+                       if(outputs[1].ScriptHash.AsBigInteger() != GetSmartContractScriptHash().AsBigInteger() )
                            return false;
                    }
                    
                    if(i>2)
-                    return false;
+                   	return false;
                    i++;
                }       
                
@@ -136,8 +135,8 @@ namespace NeoContract1
         public static bool CheckUTXO(byte[] utxoHash, BigInteger value)
         {
             Transaction TransactionOfDesiredUTXOClaim = Blockchain.GetTransaction(utxoHash);
-            // Use GetReceiver() because we want to know who much arrived to this contract
-            ulong utxoContributedValue = GetContributeValue(TransactionOfDesiredUTXOClaim, GetReceiver());
+            // Use GetSmartContractScriptHash() because we want to know who much arrived to this contract
+            ulong utxoContributedValue = GetContributeValue(TransactionOfDesiredUTXOClaim, GetSmartContractScriptHash());
             Runtime.Notify("Inside Record: Contributed value of the requested UTXO is ");
             Runtime.Notify(utxoContributedValue);
             
@@ -151,7 +150,6 @@ namespace NeoContract1
         public static void RecordTransferAndUTXO(byte[] address, BigInteger value, byte[] utxo)
         {
           //Runtime.Notify("Inside Record: Contributed value is  " + GetContributeValue((Transaction)ExecutionEngine.ScriptContainer) );
-          //Runtime.Notify("Inside Record: Sender is  " + GetSender() );
           if(!CheckUTXO(utxo,value))
           {
               Runtime.Notify("Be carefull, you should pass a valid UTXO.");    
@@ -171,25 +169,8 @@ namespace NeoContract1
           Runtime.Notify(Storage.Get( Storage.CurrentContext, utxoKey ));
         }
       
-
-        private static byte[] GetSender()
-        {
-            Transaction tx = (Transaction)ExecutionEngine.ScriptContainer;
-            TransactionOutput[] reference = tx.GetReferences();
-
-            // you can choice refund or not refund
-            foreach (TransactionOutput output in reference)
-            {
-                Runtime.Notify("Outputs");
-                Runtime.Notify(output.ScriptHash);
-                Runtime.Notify(output.ScriptHash.AsBigInteger());
-                if (output.AssetId == neo_asset_id) return output.ScriptHash;
-            }
-            return new byte[]{};
-        }
-        
         // get smart contract script hash
-        private static byte[] GetReceiver()
+        private static byte[] GetSmartContractScriptHash()
         {
             return ExecutionEngine.ExecutingScriptHash;
         }
@@ -197,9 +178,9 @@ namespace NeoContract1
         // get all you contribute neo amount
         private static ulong GetContributeValue(Transaction tx, byte[] Receiver, bool neoOrGas = true)
         {
-            byte[] assetToGetValue = neo_asset_id;
+            byte[] assetIDToGetValue = neo_asset_id;
             if(!neoOrGas)
-               assetToGetValue = neo_asset_id; // TODO - Change to GAS
+                assetIDToGetValue = gas_asset_id;
                
             TransactionOutput[] outputs = tx.GetOutputs();
             ulong value = 0;
@@ -207,7 +188,7 @@ namespace NeoContract1
             // 获取转入智能合约地址的Neo总量
             foreach (TransactionOutput output in outputs)
             {
-                if (output.ScriptHash == Receiver && output.AssetId == neo_asset_id)
+                if (output.ScriptHash == Receiver && output.AssetId == assetIDToGetValue)
                 {
                     value += (ulong)output.Value;
                 }
